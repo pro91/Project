7ac715e745a74c271be0ae24b4096c5e6b8f1da4
Rosa <pr.rosinski@gmail.com>
2019-09-07 14:14:56 +0200

    Inicjalizacja projektu

---
+++ src/main/java/com/example/Luxury/LuxuryApplication.java
@@ -0,0 +1,13 @@
+package com.example.Luxury;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuxuryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuxuryApplication.class, args);
+	}
+
+}
